8212ca15051632c655e2d12aa500fe1e044a2005
Daniel Sun Yuan <danielsunyuan@gmail.com>
2025-06-10 02:50:44 +1000

    added back in the jericho directory, simplified it away from the API format, and simply have it as our host for holding our game files

---
+++ jericho/download.sh
@@ -0,0 +1,4 @@
+mkdir -p ./jericho/games
+wget -P ./jericho/games https://github.com/BYU-PCCL/z-machine-games/archive/master.zip
+unzip ./jericho/games/master.zip -d ./jericho/games
+rm ./jericho/games/master.zip
